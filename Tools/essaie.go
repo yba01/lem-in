772cc01a92cdbfbb5764729cda8399f234432e08
yba <byero0470@gmail.com>
2024-01-05 08:02:07 +0000

    2ndstape

---
+++ Tools/essaie.go
@@ -0,0 +1,127 @@
+package Tools
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+// 1-
+// Get data from file and put it in string's slice
+func GetData(path string) []string {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println("Error Read file :", err)
+		os.Exit(0)
+	}
+	strcontent := strings.Split(string(content), "\n")
+	return strcontent
+}
+func AntNumber(tab []string) int {
+	if len(tab) > 1 {
+		number, err := strconv.Atoi(tab[0])
+		if err != nil {
+			fmt.Println("Error ant number :",err)
+			os.Exit(0)
+		}
+		return number
+	}
+	return 0
+}
+func RoomAndLinks(tab []string) ([]string,[]string) {
+	var index int
+	for i, str := range tab {
+		count := len(strings.Split(str, "-"))
+		if count == 2 {
+			index = i
+			break
+		}
+	}
+	return tab[1:index], tab[index:]
+}
+func StartAndEndRoom(tab []string) (string, string) {
+	var start []string
+	var end []string
+	for i, str := range tab {
+		if str == "##start" {
+			start = strings.Split(tab[i+1], " ")
+		}
+		if str == "##end" {
+			end = strings.Split(tab[i+1]," ")
+		}
+	}
+	return start[0], end[0]
+}
+func NameRooms(tab []string) []string {
+	var namerooms []string
+	for _, str := range tab {
+		value := strings.Split(str," ")
+		if len(value) == 3 {
+			namerooms = append(namerooms, value[0])
+		}
+	}
+	return namerooms
+}
+func LedgeBetween(tab []string) map[int][]string {
+	j:=0
+	links := make(map[int][]string)
+	for i:=0; i<len(tab);i++ {
+		value := strings.Split(tab[i], "-")
+		if len(value)==2 {
+			links[j] = value
+			j++
+		}
+	}
+	return links
+}
+//2-
+type Graph struct {
+	Node 		map[string][]string
+	StartNode 	string
+	EndNode 	string
+}
+
+func NewGraph() *Graph {
+	return &Graph{
+		Node: make(map[string][]string),
+	}
+}
+
+func (g *Graph) AddNode(node string){
+	if _, exist := g.Node[node]; !exist {
+		g.Node[node]=[]string{}
+	}else {
+		fmt.Println("invalid data format, duplicated room")
+		os.Exit(0)
+	}
+}
+func (g *Graph) AddEdge(from, to string) {
+	if _, exist :=g.Node[from]; !exist {
+		fmt.Println("invalid data format, unknown room")
+		os.Exit(0)
+	}
+	if _, exist :=g.Node[to]; !exist {
+		fmt.Println("invalid data format, unknown room")
+		os.Exit(0)
+	}
+
+	g.Node[from] = append(g.Node[from], to)
+	g.Node[to] = append(g.Node[to], from)
+}
+func (g *Graph) GetStartNode(node string) {
+	if _, exist := g.Node[node]; exist {
+		g.StartNode = node
+	}else {
+		fmt.Println("invalid data format, unknown room")
+		os.Exit(0)
+	}
+}
+func (g *Graph) GetEndNode(node string) {
+	if _, exist := g.Node[node]; exist {
+		g.EndNode = node
+	}else {
+		fmt.Println("invalid data format, unknown room")
+		os.Exit(0)
+	}
+}
--- main.go
+++ main.go
@@ -1,10 +1,44 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"main/Tools"
+	"os"
+)
 
 func main() {
 	if len(os.Args) != 2 {
 		return
 	}
-	file:=os.Args[1]
+	namefile:=os.Args[1]
+	data := Tools.GetData(namefile)
+	antnumber := Tools.AntNumber(data)
+	allrooms, allinks := Tools.RoomAndLinks(data)
+	start,end := Tools.StartAndEndRoom(allrooms)
+	namerooms := Tools.NameRooms(allrooms)
+	edge := Tools.LedgeBetween(allinks)
+	fmt.Println(data)
+	fmt.Println(antnumber)
+	fmt.Println(allrooms)
+	fmt.Println(allinks)
+	fmt.Println(start)
+	fmt.Println(end)
+	fmt.Println(namerooms)
+	fmt.Println(edge)
+	graph := Tools.NewGraph()
+	for _,node := range namerooms {
+		graph.AddNode(node)
+	}
+	for _, link := range edge {
+		graph.AddEdge(link[0], link[1])
+	}
+	graph.GetStartNode(start)
+	graph.GetEndNode(end)
+
+	fmt.Println("Graph Nodes and Edges:")
+	for node, edges := range graph.Node {
+		fmt.Printf("Node %s -> Edges: %s\n", node, edges)
+	}
+	fmt.Printf("Start Room: %s\n", graph.StartNode)
+	fmt.Printf("End Room: %s\n", graph.EndNode)
 }
