2e13790cf9dccf233c48c9736dd27f491f832833
Yero  BA <yba@noreply.learn.zone01dakar.sn>
2024-01-14 21:06:07 +0000

    New Change

--- main.go
+++ main.go
@@ -17,14 +17,14 @@ func main() {
 	start,end := Tools.StartAndEndRoom(allrooms)
 	namerooms := Tools.NameRooms(allrooms)
 	edge := Tools.LedgeBetween(allinks)
-	fmt.Println(data)
+//	fmt.Println(data)
 	fmt.Println(antnumber)
-	fmt.Println(allrooms)
-	fmt.Println(allinks)
-	fmt.Println(start)
-	fmt.Println(end)
-	fmt.Println(namerooms)
-	fmt.Println(edge)
+//	fmt.Println(allrooms)
+//	fmt.Println(allinks)
+//	fmt.Println(start)
+//	fmt.Println(end)
+//	fmt.Println(namerooms)
+//  	fmt.Println(edge)
 	graph := Tools.NewGraph()
 	for _,node := range namerooms {
 		graph.AddNode(node)
@@ -35,13 +35,28 @@ func main() {
 	graph.GetStartNode(start)
 	graph.GetEndNode(end)
 
-	fmt.Println("Graph Nodes and Edges:")
-	for node, edges := range graph.Node {
-		fmt.Printf("Node %s -> Edges: %s\n", node, edges)
-	}
-	fmt.Printf("Start Room: %s\n", graph.StartNode)
-	fmt.Printf("End Room: %s\n", graph.EndNode)
+//	fmt.Println("Graph Nodes and Edges:")
+//	for node, edges := range graph.Node {
+//		fmt.Printf("Node %s -> Edges: %s\n", node, edges)
+//	}
+//	fmt.Printf("Start Room: %s\n", graph.StartNode)
+//	fmt.Printf("End Room: %s\n", graph.EndNode)
 
 	paths := graph.FindPathBfs()
-	fmt.Println(paths)
+	//fmt.Println(paths)
+	tab := make(map[int][][]string)
+	i:=0
+	for _, chem := range paths {
+		chemins := [][]string{chem}
+		for _, str := range paths {
+			if !Tools.AvoidJams(chemins, str, graph.StartNode, graph.EndNode) {
+				chemins = append(chemins, str)
+			}
+		}
+		tab[i] = chemins
+		i += 1
+	}
+		//fmt.Println(tab)
+		solution := Tools.Maxlenght(tab)
+		fmt.Println(solution)
 }
